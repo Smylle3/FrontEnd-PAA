24a35c74d9af0f8b18492bbf8116d1945bb74df5
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-11 11:10:01 -0300

    Estilização global com styled components

---
+++ src/global.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+    --main-background-color: #eeeeee;
+    --main-button-color: #a5d6a7;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+`
