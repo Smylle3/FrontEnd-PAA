509e65285035137857c6222ad2d5739ddd802b8b
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-19 15:21:55 -0300

    Adiciona desenvolvedores no footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FooterContainer } from "./styles";
+
+export default function Footer() {
+    return (
+        <FooterContainer>
+            Desenvolvedores: Hyago Gabriel - João Pedro Rodrigues - João Pedro
+            Andrade - Thamires - Nataly Lacerda - Pedro Chaves - Gabriel Nardeli
+        </FooterContainer>
+    );
+}
