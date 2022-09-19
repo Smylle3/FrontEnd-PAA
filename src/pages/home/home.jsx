32601a5ef3a3c7c1e84fa80a573ff8b721a37e48
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-19 15:22:40 -0300

    Adiciona busca

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+/* eslint-disable array-callback-return */
+import React, { useState } from "react";
 import NewsCard from "components/newsCard/newsCard";
-import { ContainerGeneral, HeaderNews, HeaderButtons } from "./styles";
+import {
+    ContainerGeneral,
+    HeaderNews,
+    HeaderButtons,
+    SearchInput,
+} from "./styles";
 import { useNews } from "context";
 import defaultNews from "assets/defaultNews.jpg";
 import { FcNews } from "react-icons/fc";
 
 export default function Home() {
     const { news } = useNews();
+    const [search, setSearch] = useState("");
 
     return (
         <ContainerGeneral>
@@ -16,27 +23,53 @@ export default function Home() {
                         window.scrollTo({ top: 0, behavior: "smooth" })
                     }
                 >
-                    <FcNews />
-                    Suas notícias TOP!
-                    <FcNews />
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}
+                    >
+                        <FcNews />
+                        Suas notícias do BALACOBACO!
+                        <FcNews />
+                    </div>
+                    <SearchInput
+                        type="search"
+                        onChange={(e) => setSearch(e.target.value)}
+                        value={search}
+                        placeholder="Pesquisar uma notícia..."
+                    />
                 </HeaderButtons>
             </HeaderNews>
 
             {news.length > 0 ? (
                 <>
-                    {news.map((content, index) => (
-                        <NewsCard
-                            key={index}
-                            newsTitle={content.title}
-                            newsSubtitle={content.subTitle}
-                            newsImage={
-                                content.image.length > 0
-                                    ? content.image
-                                    : defaultNews
+                    {news
+                        .filter((value) => {
+                            if (search === "") {
+                                return value;
+                            } else if (
+                                value.title
+                                    .toLocaleLowerCase()
+                                    .includes(search.toLocaleLowerCase())
+                            ) {
+                                return value;
                             }
-                            id={content.id}
-                        />
-                    ))}
+                        })
+                        .map((content, index) => (
+                            <NewsCard
+                                key={index}
+                                newsTitle={content.title}
+                                newsSubtitle={content.subTitle}
+                                newsImage={
+                                    content.image.length > 0
+                                        ? content.image
+                                        : defaultNews
+                                }
+                                id={content.id}
+                            />
+                        ))}
                 </>
             ) : (
                 <h1>Carregando...</h1>
