54e0e44574b530ff5cbefcd56a33d87eca461e0b
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-19 14:33:22 -0300

    Estiliza a página de noticia

--- src/pages/newsPage/styles.js
+++ src/pages/newsPage/styles.js
@@ -8,6 +8,7 @@ export const ContainerNews = styled.div`
 
     display: flex;
     align-items: center;
+    justify-content: center;
     flex-direction: column;
     gap: 2em;
     box-sizing: border-box;
@@ -25,12 +26,22 @@ export const NewsMainImage = styled.img`
 `
 
 export const NewsMainTitle = styled.h1`
+    text-align: center;
+`
+
+export const NewsSubTitle = styled.h3`
+    text-align: center;
+    width: 70%;
 `
 
 export const NewsContent = styled.p`
     margin: 0;
     padding: 0 5em;
     text-align: justify;
+
+    @media (max-width: 930px){
+        padding: 0 2em;
+    }
 `
 
 export const BackButton = styled.button`
@@ -44,14 +55,22 @@ export const BackButton = styled.button`
     outline: 0;
     box-shadow: 3px 3px 15px -3px black;
     position: fixed;
-    left: 10%;
-    top: 25%;
+    left: 5%;
+    bottom: 5%;
+    background-color: black;
+    color: white;
     transition: all 0.3s;
     cursor: pointer;
 
     :hover{
         box-shadow: 0px 0px 5px 0px black;
+        background-color: var(--main-background-color);
+        color: black;
     }
+`
+
+export const AnimationBox = styled.div`
+    justify-self: center;
 `
 
 export const FeedBackButton = styled.button`
@@ -63,4 +82,30 @@ export const FeedBackButton = styled.button`
     outline: 0;
     cursor: pointer;
 
+`
+
+export const ListNews = styled.button`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+    width: 90%;
+    border: 1px solid black;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: .5em .5em;
+    background-color: white;
+    transition: .3s;
+    :hover{
+        box-shadow: 0 0 15px 0px black;
+    }
+`
+
+export const AncorNews = styled.a`
+    display: flex;
+    align-items: center;
+    gap: .5em;
+    font-size: large;
+    font-family: 'Times New Roman', Times, serif;
+    color: black;
 `
