1cc2892dd2e1b7345b842b49614d8aaadaab26b6
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-19 14:32:59 -0300

    Implementa a página de noticia

--- src/pages/newsPage/newsPage.jsx
+++ src/pages/newsPage/newsPage.jsx
@@ -1,56 +1,103 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { BiArrowBack } from "react-icons/bi";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BiArrowBack, BiLinkExternal } from "react-icons/bi";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
 import {
-    AiFillDislike,
-    AiFillLike,
-    AiOutlineDislike,
-    AiOutlineLike,
-} from "react-icons/ai";
-import {
+    AncorNews,
+    AnimationBox,
     BackButton,
     ContainerNews,
     FeedBackButton,
+    ListNews,
     NewsContent,
     NewsMainImage,
     NewsMainTitle,
+    NewsSubTitle,
 } from "./styles";
+import defaultNews from "assets/defaultNews.jpg";
+import api from "api/api";
 
 export default function NewsPage() {
+    const { id } = useParams();
     const navigate = useNavigate();
-    const [like, setLike] = useState(false);
+    const [newsInfo, setNewsInfo] = useState([]);
+    const [loading, setloading] = useState(false);
+    const [liked, setliked] = useState(false);
 
-    const likeNews = like ? (
-        <AiFillLike size={25} onClick={() => setLike(false)} />
-    ) : (
-        <AiOutlineLike size={25} onClick={() => setLike(true)} />
-    );
-    const dislikeNews = !like ? (
-        <AiFillDislike size={25} onClick={() => setLike(false)} />
-    ) : (
-        <AiOutlineDislike size={25} onClick={() => setLike(false)} />
-    );
+    console.log(newsInfo[1]);
+    useEffect(() => {
+        setloading(true);
+        api.get(`/articles/${id}`, {})
+            .then((res) => {
+                setNewsInfo(res.data);
+                setloading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, [liked]);
 
-    const newsInfo = {
-        mainImage:
-            "https://img.r7.com/images/rainha-elizabeth-jubileu-de-platina-02062022090216677?dimensions=771x420&",
-        titleNews: "Morre Rainha Elizabeth",
-        contentNews:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+    const sendLike = () => {
+        api.post(`/articles/${id}`, {})
+            .then((res) => {
+                setliked(!liked);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     };
 
-    return (
-        <ContainerNews>
-            <BackButton onClick={() => navigate("/")}>
-                <BiArrowBack size={20} />
-            </BackButton>
-            <NewsMainImage alt="mainIimage" src={newsInfo.mainImage} />
-            <FeedBackButton>
-                {likeNews}
-                {dislikeNews}
-            </FeedBackButton>
-            <NewsMainTitle>{newsInfo.titleNews}</NewsMainTitle>
-            <NewsContent>{newsInfo.contentNews}</NewsContent>
-        </ContainerNews>
-    );
+    const likeNews =
+        newsInfo[0]?.like === "1" ? (
+            <AiFillLike size={25} onClick={() => sendLike("0")} />
+        ) : (
+            <AiOutlineLike size={25} onClick={() => sendLike("1")} />
+        );
+
+    if (loading)
+        return (
+            <ContainerNews>
+                <AnimationBox>
+                    <h2>Carregando notícia...</h2>
+                </AnimationBox>
+            </ContainerNews>
+        );
+    else
+        return (
+            <ContainerNews>
+                <BackButton onClick={() => navigate("/")}>
+                    <BiArrowBack size={20} />
+                </BackButton>
+                <NewsMainImage
+                    alt={newsInfo?.titulo}
+                    src={
+                        newsInfo[0]?.image?.length > 0
+                            ? newsInfo[0]?.image
+                            : defaultNews
+                    }
+                />
+                <FeedBackButton>{likeNews}</FeedBackButton>
+                <NewsMainTitle>{newsInfo[0]?.title}</NewsMainTitle>
+                <NewsSubTitle>{newsInfo[0]?.subTitle}</NewsSubTitle>
+                <NewsContent>{newsInfo[0]?.paragraph}</NewsContent>
+
+                {newsInfo[0]?.like === "1" && (
+                    <>
+                        <NewsSubTitle>Notícias recomendadas</NewsSubTitle>
+                        {newsInfo[1]?.map((content, index) => (
+                            <ListNews>
+                                <AncorNews href={`/news/${content[1]}`}>
+                                    <BiLinkExternal />
+                                    {content[2]}
+                                </AncorNews>
+                                <p>
+                                    Grau de semelhança: {content[0].toFixed(2)}%
+                                </p>
+                            </ListNews>
+                        ))}
+                    </>
+                )}
+            </ContainerNews>
+        );
 }
