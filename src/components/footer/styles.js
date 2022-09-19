1af4b8dc7c2dbd5a4846833056e591d8fd32d8fc
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-19 15:22:24 -0300

    Estiliza footer

---
+++ src/components/footer/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+    background-color: var(--main-background-color);
+    border-top: 1px solid black;
+    padding: 0 3em;
+    height: 5em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
